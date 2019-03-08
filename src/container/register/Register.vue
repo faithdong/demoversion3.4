e2afa9a17660bdbe3e42ad68bb8f117f75467549
zhongxiaodong <zxdzyadmin@yonyou.com>
2019-03-08 20:30:06 +0800

    add

---
+++ src/container/register/Register.vue
@@ -0,0 +1,84 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2019-03-08 19:58:38 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2019-03-08 20:06:12
+ */
+
+<template>
+  <el-form
+    :model="ruleForm2"
+    status-icon
+    :rules="rules2"
+    ref="ruleForm2"
+    label-width="100px"
+    class="demo-ruleForm"
+  >
+    <el-form-item label="用户名" prop="name">
+      <el-input v-model="ruleForm2.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="pass">
+      <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="checkPass">
+      <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+      <el-button @click="resetForm('ruleForm2')">重置</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm2.checkPass !== "") {
+          this.$refs.ruleForm2.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm2: {
+        name: "",
+        pass: "",
+        checkPass: ""
+      },
+      rules2: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
