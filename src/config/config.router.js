0481e2dfd182e5786ec55dd5921ec25cb452ce38
zhongxd <zxdzyadmin@163.com>
2019-03-10 18:30:05 +0800

    add

--- src/config/config.router.js
+++ src/config/config.router.js
@@ -2,17 +2,21 @@
  * @Author: zhongxd 
  * @Date: 2019-03-08 23:26:09 
  * @Last Modified by: zhongxd
- * @Last Modified time: 2019-03-09 00:07:13
+ * @Last Modified time: 2019-03-10 15:47:13
  * 
  * 路由配置
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import Home from '../container/home/Home'
 import Login from '../container/login/Login'
 import Register from '../container/register/Register';
 
 Vue.use(VueRouter);
 const routes = [{
+	path:'/',
+	component:Home
+},{
 	path: '/login',
 	component: Login
 }, {
---
+++ src/container/home/Home.vue
@@ -0,0 +1,32 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2019-03-10 15:44:46 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2019-03-10 17:16:12
+ */
+
+
+<template>
+	<h1>Home Page</h1>
+</template>
+
+<script>
+	export default{
+		data(){
+
+		},
+		methods: {
+			
+		},
+		computed: {
+			
+		},
+		watch: {
+			
+		},
+	}
+</script>
+
+
+<style>
+</style>
