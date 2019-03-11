b3e872a24d2d4eb7f43d1dc5824176e95b30b7c3
zhongxiaodong <zxdzyadmin@yonyou.com>
2019-03-11 20:24:32 +0800

    add

---
+++ src/store.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2019-03-11 14:24:01 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2019-03-11 14:46:14
+ */
+
+ import Vue from 'vue';
+ import Vuex from 'vuex';
+
+
+ Vue.use(Vuex);
+
+ const strore = new Vuex.Store({
+   
+ })
+
+ export default strore;
