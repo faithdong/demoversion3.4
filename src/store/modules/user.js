a7b82f116335fdfc86a582cf72ecdf724346078e
zhongxd <zxdzyadmin@163.com>
2019-03-28 00:21:15 +0800

    mdy

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,55 +1,55 @@
 /*
- * @Author: zhongxd 
- * @Date: 2019-03-24 15:04:05 
+ * @Author: zhongxd
+ * @Date: 2019-03-24 15:04:05
  * @Last Modified by: zhongxd
- * @Last Modified time: 2019-03-24 17:05:52
- * 
+ * @Last Modified time: 2019-03-27 23:04:04
+ *
  * vuex 状态管理 --- 用户 modules
  */
 
- import axios from 'axios';
+import axios from "axios";
 
 const state = {
-	user:{},
-	status:''
+  user: {},
+  status: ""
 };
 
 const getters = {
-	isLogin:stat=>stat.status
+  isLogin: stat => stat.status
 };
 
 const actions = {
-	Login(context,user){
-		//context.commit('login',user);
-		axios.post('/user/login', user)
-        .then(resp => {
-					// eslint-disable-next-line 
-					console.log(resp);
-					context.commit('login',resp);
-          //const user = resp.data.user
-          //context.commit('auth_success', user)
-          //resolve(resp)
-        })
-        .catch(err => {
-						// eslint-disable-next-line 
-						console.log(err);
-          //commit('auth_error')
-         // reject(err)
-        })
-	}
-
+  Login(context, user) {
+    //context.commit('login',user);
+    axios
+      .post("/user/login", user)
+      .then(resp => {
+        // eslint-disable-next-line
+        console.log(resp);
+        context.commit("login", resp);
+        //const user = resp.data.user
+        //context.commit('auth_success', user)
+        //resolve(resp)
+      })
+      .catch(err => {
+        // eslint-disable-next-line
+        console.log(err);
+        //commit('auth_error')
+        // reject(err)
+      });
+  }
 };
 
 const mutations = {
-	login (state , user){
-		state.status = 'success';
-		state.user = user;
-	}
+  login(state, user) {
+    state.status = "success";
+    state.user = user;
+  }
 };
 
 export default {
-	state,
-	getters,
-	actions,
-	mutations
-}
+  state,
+  getters,
+  actions,
+  mutations
+};
