42c808bd64b20cf9250d0c7d9d01051ac42a7ada
zhongxd <zxdzyadmin@163.com>
2019-07-07 11:32:54 +0800

    subtmi

---
+++ src/components/Child-A.vue
@@ -0,0 +1,39 @@
+/*
+ * @Author: zhongxd 
+ * @Date: 2019-04-28 00:23:12 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2019-05-26 20:55:37
+ */
+
+<template>
+	<div>
+		<el-container>
+			<el-row>
+				<el-col :span="10" :offset="2">
+					<el-input-number v-model="childInput" placeholder=""></el-input-number>
+				</el-col>
+			</el-row>
+		</el-container>
+	</div>
+</template>
+
+<script>
+export default {
+	props:["parentToChildData"],
+	data(){
+		return{
+			childInput:"5",
+		}
+	},
+	methods:{
+
+	},
+	mounted(){
+		// eslint-disable-next-line
+		console.log("child-mounted");
+		// eslint-disable-next-line
+		console.log(this.parentToChildData);
+	}
+
+}
+</script>
--- src/container/login/Login.vue
+++ src/container/login/Login.vue
@@ -2,7 +2,7 @@
 * @Author: zhongxd
 * @Date: 2019-03-08 17:54:54
  * @Last Modified by: zhongxd
- * @Last Modified time: 2019-03-24 20:20:36
+ * @Last Modified time: 2019-04-28 00:38:15
 */
 
 
@@ -18,14 +18,18 @@
       <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
       <el-button @click="resetForm('ruleForm2')">重置</el-button>
     </el-form-item>
+    <ChildA :parentToChildData="needTransmitsData"></ChildA>
   </el-form>
 </template>
 
 <script>
  // import axios from "axios";
   // import { mapState, mapActions } from 'vuex'
+  import ChildA from '../../components/Child-A.vue';
   export default {
+    components:{ChildA},
     data() {
+      
       var validatePass = (rule, value, callback) => {
         if (value === "") {
           callback(new Error("请输入密码"));
@@ -34,6 +38,11 @@
         }
       };
       return {
+        tempObj:{
+          a:1,
+          b:"ceshi",
+          c:true,
+        },
         ruleForm2: {
           pass: ""
         },
@@ -42,13 +51,22 @@
             validator: validatePass,
             trigger: "blur"
           }]
-        }
+        },
+        needTransmitsData:[
+          {
+            label:"测试一",
+            value:"0"
+          }
+        ],
       };
     },
     methods: {
       submitForm(formName) {
         // eslint-disable-next-line
         console.log(formName);
+        this.tempObj.d = 5;
+        this.$set(this.tempObj,"e",6);
+        this.tempObj= Object.assign({}, this.tempObj, { d: 1, e: 2 })
         // this.$refs[formName].validate(valid => {
         //   if (valid) {
         //     alert("submit!");
@@ -90,6 +108,10 @@
       resetForm(formName) {
         this.$refs[formName].resetFields();
       }
+    },
+    mounted(){
+      // eslint-disable-next-line
+     console.log("child-mounted");
     }
   };
 </script>
